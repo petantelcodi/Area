161ab549394c5a969ff7f0a1b3420b543aecf421
Mar Canet <mar.canet@gmail.com>
2020-05-02 19:59:32 +0300

    add build

---
+++ area-peaceagreements-v3/build/precache-manifest.9c9d10ed33f838e19ded9ee31d000316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fb101bdca0c5e6a7719a44dafcf0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a129fee5624e05399c02",
+    "url": "/static/css/main.98a1d8fa.chunk.css"
+  },
+  {
+    "revision": "c18b39572f2be2502bd1",
+    "url": "/static/js/2.a3aeccd3.chunk.js"
+  },
+  {
+    "revision": "a129fee5624e05399c02",
+    "url": "/static/js/main.193e2225.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
