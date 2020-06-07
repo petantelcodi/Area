8fcbb7c12be02b078b9c9b34eda0e0fc937a4121
Mar Canet <mar.canet@gmail.com>
2020-06-07 18:13:23 +0300

    build and setup home in package

---
+++ area-peaceagreements-v3/build/precache-manifest.30a9468ccbe13d49bc874671a2ed4ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12181274340d5d84437d8f64b1df2a46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a68e9229ea200270e901",
+    "url": "./static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "e0325edf4459dc1a52ce",
+    "url": "./static/css/main.a46f46ef.chunk.css"
+  },
+  {
+    "revision": "a68e9229ea200270e901",
+    "url": "./static/js/2.4c1b869d.chunk.js"
+  },
+  {
+    "revision": "e0325edf4459dc1a52ce",
+    "url": "./static/js/main.fbc511be.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- area-peaceagreements-v3/build/service-worker.js
+++ area-peaceagreements-v3/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c9d10ed33f838e19ded9ee31d000316.js"
+  "./precache-manifest.30a9468ccbe13d49bc874671a2ed4ad7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
