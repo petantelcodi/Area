99caf9d95777eb308abb445ef2504f8875c3298c
Mar Canet <mar.canet@gmail.com>
2020-06-19 16:51:42 +0300

    add link PDF and html

---
+++ area-peaceagreements-v3/build/precache-manifest.c4acbe7a48c99dbe8d3349537266d4ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809f54d4f87b01adf1d20c91862b10a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ad60198ee9424ceef0b",
+    "url": "./static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "db5f85670aec4d6d7a08",
+    "url": "./static/css/main.a8db945b.chunk.css"
+  },
+  {
+    "revision": "2ad60198ee9424ceef0b",
+    "url": "./static/js/2.b4cbc3cd.chunk.js"
+  },
+  {
+    "revision": "db5f85670aec4d6d7a08",
+    "url": "./static/js/main.8beede32.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
